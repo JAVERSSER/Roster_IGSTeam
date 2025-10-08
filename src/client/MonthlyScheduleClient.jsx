97ee2de520bfcada9822b4ddaa2f00206c1aa961
Thirith Heng <thirithheng@gmail.com>
2025-10-08 22:01:42 +0700

    Init

--- src/client/MonthlyScheduleClient.jsx
+++ src/client/MonthlyScheduleClient.jsx
@@ -108,7 +108,7 @@ const MonthlyScheduleClient = ({ setCurrentView }) => {
             onClick={() => setCurrentView("dashboard")}
             className="text-red-500 font-medium text-sm md:text-base"
           >
-            ← Back
+            Back
           </button>
           <h1 className="text-base md:text-xl font-bold text-red-500 mx-2 text-center flex-1">
             {months[currentMonth]} {currentYear}
